d95a0441687fe67a40cc2eef31b35e7835ad6f7b
le.manh.dat <le.manh.dat@sun-asterisk.com>
2021-01-27 00:01:36 +0700

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :admin do
+    resources :categories
+    resources :posts
+  end
+
+  namespace :user, path: "" do
+    resources :posts
+  end
 end
